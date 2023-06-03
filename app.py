9db3c124b500e4de144beb3446c29186c2428460
nisrinazahran <nisrinazahran127@gmail.com>
2023-06-03 17:28:33 +0700

    modified from nisrinaz

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ def home():
 def loginAdmin():
     return render_template('login_admin.html')
 
+@app.route('/loginUser')
+def loginUser():
+    return render_template('login_user.html')
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
