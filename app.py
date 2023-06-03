9bc73d8b91e55030462f3e19d505fcebcae4b909
ariferazzak <arieferazzak7@gmail.com>
2023-06-03 14:44:44 +0700

    halaman homepage

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
+@app.route('/loginAdmin')
+def loginAdmin():
+    return render_template('login_admin.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
