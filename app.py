0af0a8407b5b1dfbca062e6c65adba4805695c4f
ariferazzak <arieferazzak7@gmail.com>
2023-06-04 11:45:20 +0700

    modifikasi by arif

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request, session, redirect, url_for
+from pymongo import MongoClient
+import jwt
+from datetime import datetime
+import hashlib
+
 app = Flask(__name__)
+koneksi_mongodb = 'mongodb://razzak:razzak08@ac-klaldor-shard-00-00.eeumlbk.mongodb.net:27017,ac-klaldor-shard-00-01.eeumlbk.mongodb.net:27017,ac-klaldor-shard-00-02.eeumlbk.mongodb.net:27017/?ssl=true&replicaSet=atlas-7byu10-shard-0&authSource=admin&retryWrites=true&w=majority'
+client = MongoClient(koneksi_mongodb)
+db = client.dbfinal_project
+
+SECRET_KEY = 'finalProject'
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def homeLogin():
+    return render_template('home.html')
 
-@app.route('/loginAdmin')
+@app.route('/login/Admin')
 def loginAdmin():
     return render_template('login_admin.html')
 
-@app.route('/loginUser')
+@app.route('/login/User')
 def loginUser():
     return render_template('login_user.html')
 
