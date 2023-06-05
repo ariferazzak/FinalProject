37a0ecabfc95cb09bcc250e76a7923cdc882c103
ariferazzak <arieferazzak7@gmail.com>
2023-06-05 22:16:40 +0700

    modif api by arif

--- app.py
+++ app.py
@@ -36,14 +36,16 @@ def registerUser():
 def api_register_admin():
     id_receive = request.form.get('id_give')
     username_receive = request.form.get('username_give')
+    alamat_receive = request.form.get('alamat_give')
     pw_receive = request.form.get('pw_give')
 
     pw_hash = hashlib.sha256(pw_receive.encode("utf-8")).hexdigest()
 
-    db.user.insert_one({
-        "id": id_receive, 
-        "pw": pw_hash, 
-        "name": username_receive})
+    db.admin.insert_one({
+        "id": id_receive,  
+        "name": username_receive,
+        "alamat": alamat_receive,
+        "pw": pw_hash,})
 
     return jsonify({"result": "success"})
 
