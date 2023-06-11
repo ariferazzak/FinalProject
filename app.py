5b901093fa5b7352c8c59d928b6aa843e3d162e2
masmuhamadjamaludin <masmuhamadjamaludin1@gmail.com>
2023-06-11 15:51:34 +0700

    route homepage admin

--- app.py
+++ app.py
@@ -181,10 +181,6 @@ def status(name):
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect('/homepage_user')
 
-@app.route('/profile/admin')
-def profile_admin():
-    return render_template('profile_admin.html')
-
 @app.route('/profile/admin',methods=['GET'])
 def profile_admin(name):
     token_receive = request.cookies.get(TOKEN_KEY)
